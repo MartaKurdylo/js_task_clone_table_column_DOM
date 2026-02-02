cc55744d1cde6b361cde610e599f01068105b308
Marta Kurdylo <martakurdylo17@gmail.com>
2026-02-02 15:36:58 +0200

    make build

---
+++ index.b89914e3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");function r(e){var r=!0,t=!1,l=void 0;try{for(var o,n=e.rows[Symbol.iterator]();!(r=(o=n.next()).done);r=!0){var a=o.value,c=a.cells[1].cloneNode(!0);a.insertBefore(c,a.cells[a.cells.length-1])}}catch(e){t=!0,l=e}finally{try{r||null==n.return||n.return()}finally{if(t)throw l}}}r(e.tHead),r(e.tBodies[0]),r(e.tFoot);
+//# sourceMappingURL=index.b89914e3.js.map
